01b5420a9367295e3cb336d35ce378a87f0175e1
Alexis Guzman <alexis12989x@gmail.com>
2020-11-26 16:42:21 -0600

    Create LocationInfoBox component

---
+++ src/components/contexts/LocationInfoBox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LocationInfoBox = () => {
+    return (
+        <div>
+            <p>LocationInfoBox Component</p>
+        </div>
+    );
+};
+
+export default LocationInfoBox;
