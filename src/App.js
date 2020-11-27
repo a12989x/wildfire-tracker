d767616d6ddc138094250dc411ba143f37da5eca
Alexis Guzman <alexis12989x@gmail.com>
2020-11-27 09:10:42 -0600

    Set up App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import { useContext } from 'react';
+import { HashLoader } from 'react-spinners';
+
+import { WildfireContext } from './components/contexts/WildfireContext';
+
+import Map from './components/Map';
+import Header from './components/Header';
 
 const App = () => {
+    const { eventData, loading } = useContext(WildfireContext);
+
     return (
         <div className='app'>
-            <p>App Component</p>
+            <Header />
+            {!loading ? (
+                <Map eventData={eventData} />
+            ) : (
+                <HashLoader loading={true} size={200} />
+            )}
         </div>
     );
 };
